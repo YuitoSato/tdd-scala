57b64833139c4e2e303822ead386cab47104686c
Yuito Sato <yuitosato.w@gmail.com>
2018-08-08 10:57:16 +0900

    ex1

---
+++ src/test/scala/co/yuito/fizzbuzz/FizzBuzzSpec.scala
@@ -0,0 +1,24 @@
+package co.yuito.fizzbuzz
+
+import org.scalatest.FunSpec
+
+class FizzBuzzSpec extends FunSpec {
+
+  describe("#run") {
+    Seq(
+      ("1", 1),
+      ("Fizz", 3),
+      ("Fizz", -3),
+      ("Fizz", 6),
+      ("Buzz", 5),
+      ("Buzz", 10),
+      ("FizzBuzz", 15),
+      ("FizzBuzz", 30)
+    ).foreach { case (expected, x) =>
+      it(s"value = $x , expect = $expected") {
+        assert(FizzBuzz.run(x) == expected)
+      }
+    }
+  }
+
+}
