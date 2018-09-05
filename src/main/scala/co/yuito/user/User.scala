666e00b3ea3dd23b687483c4365d49e21d8a631d
Yuito Sato <yuitosato.w@gmail.com>
2018-09-05 17:48:54 +0900

    tableを使ってリファクタ

--- src/main/scala/co/yuito/user/User.scala
+++ src/main/scala/co/yuito/user/User.scala
@@ -1,9 +1,18 @@
 package co.yuito.user
 
-case class User(password: String, name: String) {
+case class User(
+  password: String,
+  name: String,
+  companyNameOpt: Option[String],
+  mailAddressOpt: Option[String]
+) {
 
   def validate: Boolean = password.length >= 8
 
-  def getSignature: String = s"$name OK"
+  def getSignature: String = {
+    val result = if (validate) "OK" else "NG"
+    val companyName = companyNameOpt.fold("")(_ + ": ")
+    companyName + name + " " + result
+  }
 
 }
--- src/test/scala/co/yuito/user/UserSpec.scala
+++ src/test/scala/co/yuito/user/UserSpec.scala
@@ -6,35 +6,103 @@ import org.scalatest.prop.TableDrivenPropertyChecks._
 class UserSpec extends FunSpec {
 
   describe(".validate") {
-    val fractions = Table(
-      ("password", "result"),
-      ("password", true),
-      ("passwor", false)
+    val table = Table(
+      ("description", "password", "expected"),
+      ("パスワードが8文字以上の時", "password", true),
+      ("パスワードが7文字以下の時", "passwor", false)
     )
-    forAll (fractions) { (password , result) =>
-      describe(s"パスワードが${password.length}文字のとき") {
-        it(s"${result}を返す") {
-          val user = User(password, "name")
-          assert(user.validate == result)
+
+    forAll (table) { (description, password, expected) =>
+      describe(description) {
+        it(s"${expected}を返す") {
+          val user = User(password, "name", None, None)
+          assert(user.validate == expected)
         }
       }
     }
   }
 
   describe(".getSignature") {
-    describe("会社名がない場合は氏名のみ表示") {
-      it("署名を返す") {
-        val name = "氏名"
-        val user = User("password", name)
-        assert(user.getSignature == s"$name OK")
-      }
+    val table = Table(
+      ("description", "behavior", "user", "expected"),
+      ("パスワードが正しい時", "末尾にOKが入る。", User("password", "name", None, None), "name OK"),
+      ("パスワードが不正な時", "末尾にNGが入る。", User("passwor", "name", None, None), "name NG"),
+      ("会社名と氏名が両方存在する時", "会社名 + 氏名 + 検証結果を署名として返す。", User("password", "name", Some("companyName"), None), "companyName: name OK"),
+      ("氏名のみ存在する時", "氏名 + 検証結果を署名として返す。", User("password", "name", None, None), "name OK")
+    )
 
-      it("氏名2 OK を返す") {
-        val name = "氏名2"
-        val user = User("password", name)
-        assert(user.getSignature == s"$name OK")
+    forAll (table) { (description, behavior, user, expected) =>
+      describe(description) {
+        it(s"${behavior + "「" + expected}」を返す") {
+          assert(user.getSignature == expected)
+        }
       }
     }
   }
+
+
+//  describe(".getSignature") {
+//    describe("検証結果が正しい場合") {
+//      Seq(
+//        (User(password = "password", name = "氏名"), "氏名 OK"),
+//        (User(password = "password", name = "氏名2"), "氏名2 OK"),
+//        (User(password = "password", name = ""), " OK")
+//      ).foreach { case (user, expected) =>
+//        describe("氏名のみ存在する場合") {
+//          it(s"$expected を返す") {
+//            assert(user.getSignature == expected)
+//          }
+//        }
+//      }
+//
+//      Seq(
+//        (User(password = "password", name = "氏名", companyName = "BizReach"), "BizReach: 氏名 OK")
+//      ).foreach { case (user, expected) =>
+//        describe("氏名と会社名存在する場合") {
+//          it(s"$expected を返す") {
+//            assert(user.getSignature == expected)
+//          }
+//        }
+//      }
+//    }
+//
+//    describe("検証結果が正しくない場合") {
+//      Seq(
+//        (User(password = "passwor", name = "氏名"), "氏名 NG")
+//      ).foreach { case (user, expected) =>
+//        it(s"$expected を返す") {
+//          assert(user.getSignature == expected)
+//        }
+//      }
+//    }
+//
+//    describe("検証結果が失敗の場合") {
+//      it("署名にNGを返す") {
+//        val name = "氏名"
+//        val companyName = "BizReach"
+//        val mailAddress = "hrmos@bizreach.co.jp"
+//        val user = User(password = "passwor", name = name, companyName = companyName, mailAddress = mailAddress)
+//        assert(user.getSignature == s"$companyName: $name NG")
+//      }
+//    }
+//
+//    describe("メールアドレスを登録する") {
+//      it("メールアドレスが存在する") {
+//        val name = "氏名"
+//        val companyName = "BizReach"
+//        val mailAddress = "hrmos@bizreach.co.jp"
+//        val user = User(password = "passwor", name = name, companyName = companyName, mailAddress = mailAddress)
+//        assert(user.hasMailaddress)
+//      }
+//
+//      it("メールアドレスが存在しない") {
+//        val name = "氏名"
+//        val companyName = "BizReach"
+//        val mailAddress = ""
+//        val user = User(password = "passwor", name = name, companyName = companyName, mailAddress = mailAddress)
+//        assert(!user.hasMailaddress)
+//      }
+//    }
+//  }
 
 }
